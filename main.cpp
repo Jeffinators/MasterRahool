e59a9534f465bdc8275cb2677f637732b98aef03
Thomas Rua <tomr6529@gmail.com>
2020-12-02 15:22:53 -0500

    main app

---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <string>
+int main()
+{
+	int numbcommon = 1;
+	int numblegendary = 2;
+	std::string weapontype[] = { "Auto Rifle", "Scout Rifle", "Bow", "Sub-machine Gun", "Sidearm", "Handcannon", "Shotgun", "Sniper", "Breech-Loaded Grenade Launcher", "Fusion Rifle", "Trace Rifle", "Rocket Launcher", "Gernade Launcher", "Sword", "Machine Gun", "Linear Fusion Rifel", };
+	std::cout << weapontype[0] << "\n";
+
+	return 0;
+}
