dae9cf59e0785cef8c4e06c922cb8e396a6ca621
Thomas Rua <tomr6529@gmail.com>
2020-12-03 08:09:08 -0500

    main app/backbone

--- main.cpp
+++ main.cpp
@@ -8,7 +8,7 @@ int main()
 	int randnumb;
 	string weapon;
 	string weapontype[] = { "Auto Rifle", "Scout Rifle", "Bow", "Sub-machine Gun", "Sidearm", "Handcannon", "Shotgun", "Sniper", "Breech-Loaded Grenade Launcher", "Fusion Rifle", "Trace Rifle", "Rocket Launcher", "Gernade Launcher", "Sword", "Machine Gun", "Linear Fusion Rifel" };
-
+	
 	for (int i = 0; i < numbcommon; i++) {
 		randnumb = (rand() % 15);
 		weapon = weapontype[randnumb];
