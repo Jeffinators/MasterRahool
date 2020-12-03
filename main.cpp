935b4c72bf427a5d87868a3f6e2beb18e3b48e97
Thomas Rua <tomr6529@gmail.com>
2020-12-03 07:38:29 -0500

    main app

--- main.cpp
+++ main.cpp
@@ -1,11 +1,18 @@
 #include <iostream>
 #include <string>
+using namespace std;
 int main()
 {
 	int numbcommon = 1;
 	int numblegendary = 2;
-	std::string weapontype[] = { "Auto Rifle", "Scout Rifle", "Bow", "Sub-machine Gun", "Sidearm", "Handcannon", "Shotgun", "Sniper", "Breech-Loaded Grenade Launcher", "Fusion Rifle", "Trace Rifle", "Rocket Launcher", "Gernade Launcher", "Sword", "Machine Gun", "Linear Fusion Rifel", };
-	std::cout << weapontype[0] << "\n";
+	int randnumb;
+	string weapon;
+	string weapontype[] = { "Auto Rifle", "Scout Rifle", "Bow", "Sub-machine Gun", "Sidearm", "Handcannon", "Shotgun", "Sniper", "Breech-Loaded Grenade Launcher", "Fusion Rifle", "Trace Rifle", "Rocket Launcher", "Gernade Launcher", "Sword", "Machine Gun", "Linear Fusion Rifel" };
 
+	for (int i = 0; i < numbcommon; i++) {
+		randnumb = (rand() % 15);
+		weapon = weapontype[randnumb];
+		cout << weapontype[randnumb] << endl;
+	}
 	return 0;
 }
